85fea6e6dbdf8e053c41358f784ee61421828900
Zulqarnai <mzulqarnainceh@gmail.com>
2026-08-10 15:36:17 +0500

    corrected mistake in query (AUTOICREMENT), removed space between auto increment

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ def init_db():
     conn=sqlite3.connect(path)
     conn.execute("""
     CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTO INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL
