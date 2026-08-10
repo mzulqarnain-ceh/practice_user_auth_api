9d8acbe7690b8a6f6a15bf46e140816ed8682f01
Zulqarnai <mzulqarnainceh@gmail.com>
2026-08-10 12:22:49 +0500

    added database skeleton

---
+++ models.py
@@ -0,0 +1,8 @@
+import os,sqlite3
+folder=os.path.dirname(__file__)
+path=os.path.join(folder,"database.db")
+# database initialization and created table
+def init_db():
+    pass
+def get_db_connection():
+    pass
