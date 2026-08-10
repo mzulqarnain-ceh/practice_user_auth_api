02f6c6d62b6236b8bdc5c7666e1e5459689de308
Zulqarnai <mzulqarnainceh@gmail.com>
2026-08-10 15:39:05 +0500

    corrected mistakes: 1 pranthesis missing in lower, 2 plural list of string used (algorithms) not algorithm

--- auth.py
+++ auth.py
@@ -11,11 +11,11 @@ def token_required(f):
         if not token:
             return jsonify({"error":"Token is missing"}),401
         parts=token.split(" ")
-        if len(parts) !=2 or parts[0].lower != "bearer":
+        if len(parts) !=2 or parts[0].lower() != "bearer":
             return jsonify({"error":"Invalid token format. Must be 'Bearer <token>'"}),401
         token=parts[1]
         try:
-            payload=jwt.decode(token,SECRET_KEY,algorithm=["HS256"])
+            payload=jwt.decode(token,SECRET_KEY,algorithms=["HS256"])
         except jwt.ExpiredSignatureError:
             return jsonify({"error":"Token Expired"}),401
         except jwt.InvalidTokenError:
